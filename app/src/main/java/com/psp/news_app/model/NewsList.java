20b881f4b142a3eeba19f9e8b64b8ef36596fd25
Admin <prasad.parshram123@gmai.com>
2022-03-21 20:12:46 +0530

    added models in model resource

--- app/src/main/java/com/psp/news_app/model/NewsList.java
+++ app/src/main/java/com/psp/news_app/model/NewsList.java
@@ -1,4 +1,40 @@
 package com.psp.news_app.model;
 
+import java.util.ArrayList;
+
 public class NewsList {
+
+    public String category;
+    public ArrayList<News> data;
+    public boolean success;
+
+    public NewsList(String category, ArrayList<News> data, boolean success) {
+        this.category = category;
+        this.data = data;
+        this.success = success;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public ArrayList<News> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<News> data) {
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
