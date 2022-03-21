5ab8eef6410145d90de94b18723a73c7332a1c2b
Admin <prasad.parshram123@gmai.com>
2022-03-21 20:13:44 +0530

    added Retrofit in network resource

--- app/src/main/java/com/psp/news_app/network/Api.java
+++ app/src/main/java/com/psp/news_app/network/Api.java
@@ -1,4 +1,15 @@
 package com.psp.news_app.network;
 
+import com.psp.news_app.model.NewsList;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface Api {
+
+    @GET ("news?category=sports")
+    Call<NewsList> getNewsList();
+
 }
--- app/src/main/java/com/psp/news_app/network/RetrofitInstance.java
+++ app/src/main/java/com/psp/news_app/network/RetrofitInstance.java
@@ -1,4 +1,23 @@
 package com.psp.news_app.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+
+    // Base URL
+    private final static String BASE_URL = "https://inshortsapi.vercel.app/"; // news?category=sports
+
+    // Retrofit object
+    private static Retrofit retrofit;
+
+    public static synchronized Retrofit getInstance() {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
