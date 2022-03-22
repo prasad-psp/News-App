384d3421defb5b97228abf1db0767290c7932b05
Admin <prasad.parshram123@gmail.com>
2022-03-22 18:14:26 +0530

    implemented repo in NewsViewModel.java

--- app/src/main/java/com/psp/news_app/viewmodel/NewsViewModel.java
+++ app/src/main/java/com/psp/news_app/viewmodel/NewsViewModel.java
@@ -2,30 +2,30 @@ package com.psp.news_app.viewmodel;
 
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.psp.news_app.model.News;
-import com.psp.news_app.model.NewsList;
-import com.psp.news_app.network.Api;
-import com.psp.news_app.network.RetrofitInstance;
+import com.psp.news_app.repo.NewsRepo;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class NewsViewModel extends ViewModel {
 
+    // News list
     private final MutableLiveData<List<News>> newsList;
 
+    // Progressbar
     private final MutableLiveData<Integer> progressbar;
+
+    // Repo object
+    private final NewsRepo repo;
 
     public NewsViewModel() {
         newsList = new MutableLiveData<>();
         progressbar = new MutableLiveData<>();
+        repo = new NewsRepo();
 
         // Hide progressbar
         progressbar.setValue(View.INVISIBLE);
@@ -43,28 +43,19 @@ public class NewsViewModel extends ViewModel {
         // show progressbar
         progressbar.setValue(View.VISIBLE);
 
-        Api api = RetrofitInstance.getInstance().create(Api.class);
-        Call<NewsList> call = api.getNewsList();
-
-        call.enqueue(new Callback<NewsList>() {
+        repo.requestNewsData(new NewsRepo.OnNewsCallback() {
             @Override
-            public void onResponse(Call<NewsList> call, @NonNull Response<NewsList> response) {
+            public void onResponse(ArrayList<News> newsArrayList) {
                 // Hide progressbar
                 progressbar.setValue(View.INVISIBLE);
-
-                NewsList list = response.body();
-
-                if(list != null) {
-                    newsList.setValue(list.getData());
-                } else {
-                    newsList.setValue(null);
-                }
+                newsList.setValue(newsArrayList);
             }
 
             @Override
-            public void onFailure(Call<NewsList> call, Throwable t) {
+            public void onFailure() {
                 // Hide progressbar
                 progressbar.setValue(View.INVISIBLE);
+                newsList.setValue(null);
             }
         });
     }
